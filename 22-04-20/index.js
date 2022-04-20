7b2127aaef2c4c61840c815f707f847e8d2680c7
Holger <holgeraliusklose@gmail.com>
2022-04-20 13:09:09 +0200

    object tasks

--- 22-04-20/index.js
+++ 22-04-20/index.js
@@ -42,6 +42,7 @@ obj["name"] = "Olga";
 console.log(obj);
 obj.name = "Jeanna";
 console.log(obj);
+
 // Accessing properties
 // 1- with (bracket notation)
 
@@ -51,13 +52,73 @@ console.log(obj);
 
 // Assigning using bracket notation, more used for an empty object
 
+const userObj = {
+  name: "Tobi",
+  age: 33,
+  add: "xxx str 13040 Berlin",
+  act: ["draw", "sleep", "eating"],
+  job: "HR",
+  fat: true,
+};
+
 // Tell us more about you
 
-// Object.keys
+const privatObj = {
+  firstName: "Holger",
+  lastName: "Klose",
+  age: 35,
+  hobbysSports: ["biking", "climbing", "running"],
+  hobbysArt: ["drumming @ Mein Neues Nicki"],
+  city: "Leipzig",
+  degree: "Bachelor of Engineering",
+  university: "University of Applied Science Mittweida",
+  job: "training for Web Development",
+  currentGoal: [
+    "become a WebDevelopper",
+    "mindfulness",
+    "400km by bike in 24h",
+    "run a half marathon",
+  ],
+  bike: {
+    type: "Randonneur",
+    color: "black",
+    km: 50000,
+  },
+  display: function () {
+    // function innerhalb eines Objects wird methode gennant
+    console.log(
+      `hey ${this.firstName}, you are ${this.age}. I hope you have fun in ${this.city}`
+    );
+  },
+};
+console.log(privatObj);
+console.log("---------1----------");
+console.log(privatObj.currentGoal[2]);
+console.log("---------2----------");
+console.log(privatObj.bike.type);
+console.log("---------3----------");
+privatObj.display();
+console.log("----------4---------");
+privatObj.print = function () {
+  console.log("This is a cool method");
+};
+console.log("-----------5--------");
+console.log(privatObj);
+console.log("-----------6--------");
+privatObj.print();
+console.log("----------7---------");
 
-// Object.values
+// Object.keys gibt alle Parameter (Keys) des objects aus
+console.table(Object.keys(privatObj));
+
+// Object.values -> gibt alle Eingaben (values) des objects aus
+console.log(Object.values(privatObj));
 
 // Object.assign
+const obj1 = { kids: 2, bikes: 5 };
+const obj2 = { colors: ["red", "orange"], pet: false };
+const totalObj = Object.assign({ name: "steel", age: 25 }, obj1, obj2);
+console.log(totalObj);
 
 // Object.defineProperty()
 
@@ -66,3 +127,10 @@ console.log(obj);
 // Object.hasOwnProperty()
 
 // Object.seal()
+
+Object.seal(privatObj);
+privatObj.age = 33;
+console.log(privatObj);
+delete privatObj.name;
+console.log(privatObj);
+console.log(Object.isSealed(privatObj));
---
+++ 22-04-20/tasks.js
@@ -0,0 +1,37 @@
+// Create a function that returns an array of the properties of a javascript object.
+//   Example
+const person = {
+  name: "Mike",
+  class: "4A",
+  course: "English",
+};
+// Expected output:
+//   ["name", "class", "course"]
+
+// Given the following object
+const student = {
+  firstName: "Zain",
+  lastName: "Oil",
+  class: "D05",
+};
+// Create a method that prints the following: "Zain Oil a student in class D05"
+
+// Get Values. Create a function that returns an array of all values of an object’s properties.
+//   Examples:
+const getObjectValues = {
+  choice1: "tea",
+  choice2: "coffee",
+  choice3: "milk",
+};
+
+// Check if a number is within a given range. Write a program that checks if a number is within the range of an object's min and max properties. Assume min <= max is always true.
+
+// Examples:
+
+// (4, { min: 0, max: 5 }) ➞ true
+
+// (4, { min: 4, max: 5 }) ➞ true
+
+// (4, { min: 6, max: 10 }) ➞ false
+
+// (5, { min: 5, max: 5 }) ➞ true
