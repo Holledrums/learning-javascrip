c2b4e7bc4fe8a8656ef665e9683d3736c68dc741
Holger <holgeraliusklose@gmail.com>
2022-04-28 09:49:02 +0200

    do while, while

--- 22-04-28/index.js
+++ 22-04-28/index.js
@@ -1,1 +1,36 @@
 // advanced array method pt. II
+
+//for
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+const personObj = {
+  firstName: "Steel",
+  age: 35,
+};
+for (const prop in personObj) {
+  console.log(`${prop} : ${personObj[prop]}`);
+}
+console.log("------ for in -------");
+
+const namesArr = ["Nancy", "Ali", "Olga", "steel"];
+console.log("------ for of -------");
+for (const name of namesArr) {
+  console.log(name);
+}
+
+console.log("------ do while -------");
+// do while
+console.log("------ while -------");
+// while
+
+// forEach
+
+// find
+
+// findIndexOf
+
+// some
+
+// javascript Overview
