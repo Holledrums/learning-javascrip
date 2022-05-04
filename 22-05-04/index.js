04df53a334c99fc3061742265142dca41f6d3268
Holger <holgeraliusklose@gmail.com>
2022-05-04 14:58:37 +0200

    some tasks

--- 22-05-04/index.js
+++ 22-05-04/index.js
@@ -1,1 +1,32 @@
 // Arrays + Js test
+// setp = -step
+let x = 10;
+x = 10;
+x = -x;
+console.log(x);
+// Array
+const names = ["Ali", "Nancy", "Steel", "Olga"];
+console.log("length", names.length);
+console.log(names[2]);
+console.table(names);
+
+const ppl = [
+  { name: "Ali", age: 22, add: "Berlin" },
+  { name: "Nancy", age: 45, add: "LA" },
+  { name: "Steel", age: 29, add: "NY" },
+  { name: "Olga", age: 60, add: "Amsterdam" },
+  { name: "Leo", age: 99, add: "London" },
+];
+
+console.log(ppl[3].add);
+console.log(ppl[3]["add"]);
+console.table(ppl.map((person) => person.add));
+let addArr = [];
+for (let i = 0; i < ppl.length; i++) {
+  addArr.push(ppl[i].add);
+}
+console.table(addArr);
+
+console.table(ppl.map((pplAge) => pplAge.age));
+// Filter
+console.log(ppl.filter((person) => person.add.toLowerCase() == "berlin"));
---
+++ 22-05-04/tasks.js
@@ -0,0 +1,47 @@
+// Create a function that returns the given argument, but by using an arrow function.
+
+// An arrow function is constructed like so:
+
+// arrowFunc=(/*parameters*/)=>//code here
+// Examples
+// arrowFunc(3) ➞ 3
+
+// arrowFunc("3") ➞ "3"
+
+// arrowFunc(true) ➞ true
+
+const arrowFunc = (arg) => console.log(arg);
+arrowFunc(3);
+arrowFunc("3");
+arrowFunc(true);
+console.log("----------------");
+// A vehicle needs 10 times the amount of fuel than the distance it travels. However, it must always carry a minimum of 100 fuel before setting off.
+
+// Create a function which calculates the amount of fuel it needs, given the distance.
+
+// Examples
+// calculateFuel(15) ➞ 150
+
+// calculateFuel(23.5) ➞ 235
+
+// calculateFuel(3) ➞ 100
+
+const calculateFuel = (num) =>
+  num * 10 >= 100 ? console.log(num * 10) : console.log(100);
+calculateFuel(15);
+calculateFuel(23.5);
+calculateFuel(3);
+console.log("----------------");
+// You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.
+
+// Examples
+// points(1, 1) ➞ 5
+
+// points(7, 5) ➞ 29
+
+// points(38, 8) ➞ 100
+
+const points = (x, y) => console.log(x * 2 + y * 3);
+points(1, 1);
+points(7, 5);
+points(38, 8);
