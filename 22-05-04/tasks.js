b5eedb05364b118c19dd43b326951908171d3fee
Holger <holgeraliusklose@gmail.com>
2022-05-04 15:51:23 +0200

    some more tasks

--- 22-05-04/tasks.js
+++ 22-05-04/tasks.js
@@ -45,3 +45,27 @@ const points = (x, y) => console.log(x * 2 + y * 3);
 points(1, 1);
 points(7, 5);
 points(38, 8);
+
+// Create a function that takes an array containing only numbers and return the first element.
+
+const getFirstValue = (num) => {
+  console.log(num[0]);
+};
+
+//Examples
+getFirstValue([1, 2, 3]); // ➞ 1
+
+getFirstValue([80, 5, 100]); // ➞ 80
+
+getFirstValue([-500, 0, 50]); // ➞ -500
+
+// Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+const nextEdge = (x, y) => console.log(x + y - 1);
+
+// Examples
+nextEdge(8, 10); // ➞ 17
+
+nextEdge(5, 7); // ➞ 11
+
+nextEdge(9, 2); // ➞ 10
