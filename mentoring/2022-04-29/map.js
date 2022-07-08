533d535e371397249f48210a35c677f92496a4a3
Holger <holgeraliusklose@gmail.com>
2022-07-08 10:35:38 +0200

    changes

--- mentoring/2022-04-29/map.js
+++
@@ -1,4 +0,0 @@
-const myNumbers = [2, 5, 3, 6, 8, 9, 7];
-const result = myNumbers.map((element) => element * 3);
-console.log(result);
-// das ausgegebene Array ist nur eine Kopie des bestehenden Array mit veränderten werten.
